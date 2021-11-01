227c123213c12cb4f7579491e0d33422dd21bc2f
vontedduchaithra <90314288+vontedduchaithra@users.noreply.github.com>
2021-11-01 19:27:25 +0530

    Add files via upload

---
+++ third.js
@@ -0,0 +1,8 @@
+var x;
+console.log(x);
+console.log("Here x is undefined");
+var z=85;
+if(z==undefined)
+console.log("z is undefined");
+else
+console.log("value of z is: "+z +" and it is defined");
